fb9b474bbd9217b159140e6ad2778389a3339087
Chibueze <chibuezeo@spacekrasher.com>
2025-02-11 12:14:14 +0100

    building

--- src/components/NetworkNewsFeed.tsx
+++ src/components/NetworkNewsFeed.tsx
@@ -5,29 +5,29 @@ import Reveal from "./Reveal";
 const NetworkNewsFeed = () => {
 	return (
 		<Reveal>
-			<div className="flex flex-col justify-center items-center gap-y-5 py-16">
+			<div className="flex flex-col justify-center items-center gap-y-5 py-16 px-3 w-full">
 				<h1 className="font-Roboto text-[30px] md:text-[40px]">Network News Feed</h1>
 
-				<form action="submit">
-					<div className="flex justify-center items-center w-[700px] gap-x-4">
-						<div className="relative w-full">
-							<input
-								type="text"
-								className="p-2 peer w-full placeholder-transparent text-black bg-white outline-none caret-black peer border border-gray-200 rounded-md focus:shadow-md dark:caret-gray-400 dark:bg-transparent dark:border dark:border-gray-900 dark:focus:border-gray-800 dark:focus:shadow-slate-900"
-							/>
-							<label
-								htmlFor="name"
-								className="absolute top-0 left-0
+				<form
+					action="submit"
+					className="w-full flex flex-col items-start gap-4 md:flex-row md:items-center lg:w-[700px]">
+					<div className="relative w-full">
+						<input
+							type="text"
+							className="p-2 peer w-full placeholder-transparent text-black bg-white outline-none caret-black peer border border-gray-200 rounded-md focus:shadow-md dark:caret-gray-400 dark:bg-transparent dark:border dark:border-gray-900 dark:focus:border-gray-800 dark:focus:shadow-slate-900"
+						/>
+						<label
+							htmlFor="name"
+							className="absolute top-0 left-0
 								text-gray-400 text-sm p-2 pointer-events-none peer-placeholder-shown:top-1 peer-focus:-top-8
 								peer-focus:text-gray-800 duration-500 dark:peer-placeholder-shown:text-gray-700 dark:text-gray-700 dark:peer-focus:text-gray-400">
-								Enter RSS feed URL
-							</label>
-						</div>
-						<button className="flex justify-center items-center px-5 py-3 rounded-md bg-[#006837] text-xs font-OpenSans text-white hover:bg-[#00A651] duration-300">
-							<SquareMousePointer className="w-6 h-6 mr-1" />
-							Fetch
-						</button>
+							Enter RSS feed URL
+						</label>
 					</div>
+					<button className="flex justify-center items-center px-5 py-3 rounded-md bg-[#006837] text-xs font-OpenSans text-white hover:bg-[#00A651] duration-300">
+						<SquareMousePointer className="w-6 h-6 mr-1" />
+						Fetch
+					</button>
 				</form>
 			</div>
 		</Reveal>
--- src/components/SiteChecker.tsx
+++ src/components/SiteChecker.tsx
@@ -6,29 +6,29 @@ import Reveal from "./Reveal";
 const SiteChecker = () => {
 	return (
 		<Reveal>
-			<div className="flex flex-col justify-center items-center gap-y-5 py-16">
+			<div className="flex flex-col justify-center items-center gap-y-5 px-3 py-16">
 				<h1 className="font-Roboto text-[30px] md:text-[40px]">Website Performance Checker</h1>
 
-				<form action="submit">
-					<div className="flex justify-center items-center w-[700px] gap-x-4">
-						<div className="relative w-full">
-							<input
-								type="text"
-								className="p-2 peer w-full placeholder-transparent text-black bg-white outline-none caret-black peer border border-gray-200 rounded-md focus:shadow-md dark:caret-gray-400 dark:bg-transparent dark:border dark:border-gray-900 dark:focus:border-gray-800 dark:focus:shadow-slate-900"
-							/>
-							<label
-								htmlFor="name"
-								className="absolute top-0 left-0
+				<form
+					action="submit"
+					className="w-full flex flex-col items-start gap-4 md:flex-row md:items-center lg:w-[700px]">
+					<div className="relative w-full">
+						<input
+							type="text"
+							className="p-2 peer w-full placeholder-transparent text-black bg-white outline-none caret-black peer border border-gray-200 rounded-md focus:shadow-md dark:caret-gray-400 dark:bg-transparent dark:border dark:border-gray-900 dark:focus:border-gray-800 dark:focus:shadow-slate-900"
+						/>
+						<label
+							htmlFor="name"
+							className="absolute top-0 left-0
 								text-gray-400 text-sm p-2 pointer-events-none peer-placeholder-shown:top-1 peer-focus:-top-8
 								peer-focus:text-gray-800 duration-500 dark:peer-placeholder-shown:text-gray-700 dark:text-gray-700 dark:peer-focus:text-gray-400">
-								Enter website URL
-							</label>
-						</div>
-						<button className="flex justify-center items-center px-5 py-3 rounded-md bg-[#006837] text-xs font-OpenSans text-white hover:bg-[#00A651] duration-300">
-							<Globe className="w-6 h-6 mr-1" />
-							Check
-						</button>
+							Enter website URL
+						</label>
 					</div>
+					<button className="flex justify-center items-center px-5 py-3 rounded-md bg-[#006837] text-xs font-OpenSans text-white hover:bg-[#00A651] duration-300">
+						<Globe className="w-6 h-6 mr-1" />
+						Check
+					</button>
 				</form>
 			</div>
 		</Reveal>
